47b465ab57fbed2782903cc5b717488d18eb82a3
Nils-Kelley <77290009+Nils-Kelley@users.noreply.github.com>
2022-11-08 09:57:11 -0800

    Fixed bug for sort_values

--- notebooks/project_functions3.py
+++ notebooks/project_functions3.py
@@ -10,6 +10,7 @@ def load_and_process(url_or_path_to_csv_file):
     ren = {'Walc': 'Weekend_Alc_Consumption', 'Dalc': 'Daily_Alc_Consumption', 'freetime': 'Freetime', 'goout': 'Go_Out',
     'age': 'Age', 'school':'School', 'sex': 'Sex', 'health': 'Health', 'absences': 'Absences', 'G1': 'Period_1_Grade', 
     'G2': 'Period_2_Grade', 'G3': 'Final_Grade', 'studytime': 'Study_Time'}
+    sort = {'Weekend_Alc_Consumption','Daily_Alc_Consumption','Freetime', 'G3'}
     rep = {'F': 'Female', 'M': 'Male', 'GP': 'Gabriel Pereira', 'MS': 'Mousinho Da Silveira'}
 
     df1 = (   
@@ -17,7 +18,7 @@ def load_and_process(url_or_path_to_csv_file):
         .rename(columns=ren)
         .replace(rep)
         .drop(drop_col, axis=1)
-        .sort_values(ren, ascending=True)
+        .sort_values(sort, ascending=True)
     )
 
 
