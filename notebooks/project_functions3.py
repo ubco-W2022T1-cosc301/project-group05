5a8b813bc27ad213e779866c71c7c9ed987a6dcd
Nils-Kelley <77290009+Nils-Kelley@users.noreply.github.com>
2022-11-08 15:43:30 -0800

    Fixing bug/minor comments added to function.py

--- notebooks/project_functions3.py
+++ notebooks/project_functions3.py
@@ -5,12 +5,16 @@ def load_and_process(url_or_path_to_csv_file):
 
     # Method Chain 1 (Load data and deal with missing data)
    
+    # dropping col
     drop_col = ['traveltime','failures', 'nursery','guardian', 'Medu', 'Fedu', 'reason', 'schoolsup', 'famsup', 'paid','Fjob','Mjob', 'famrel', 'famsize', 'Pstatus'
         ,'higher', 'romantic', 'internet', 'address', 'activities']
+    # renaming col
     ren = {'Walc': 'Weekend_Alc_Consumption', 'Dalc': 'Daily_Alc_Consumption', 'freetime': 'Freetime', 'goout': 'Go_Out',
     'age': 'Age', 'school':'School', 'sex': 'Sex', 'health': 'Health', 'absences': 'Absences', 'G1': 'Period_1_Grade', 
     'G2': 'Period_2_Grade', 'G3': 'Final_Grade', 'studytime': 'Study_Time'}
+    # col to be sorted
     sort = ['Weekend_Alc_Consumption','Daily_Alc_Consumption','Freetime', 'Final_Grade']
+    # rephrase value of col
     rep = {'F': 'Female', 'M': 'Male', 'GP': 'Gabriel Pereira', 'MS': 'Mousinho Da Silveira'}
 
     df1 = (   
