690800a8d24722d34ee178747294aae1c53b2d44
Nils-Kelley <77290009+Nils-Kelley@users.noreply.github.com>
2022-11-08 10:02:37 -0800

    Fixed minor bug

--- notebooks/project_functions3.py
+++ notebooks/project_functions3.py
@@ -10,7 +10,7 @@ def load_and_process(url_or_path_to_csv_file):
     ren = {'Walc': 'Weekend_Alc_Consumption', 'Dalc': 'Daily_Alc_Consumption', 'freetime': 'Freetime', 'goout': 'Go_Out',
     'age': 'Age', 'school':'School', 'sex': 'Sex', 'health': 'Health', 'absences': 'Absences', 'G1': 'Period_1_Grade', 
     'G2': 'Period_2_Grade', 'G3': 'Final_Grade', 'studytime': 'Study_Time'}
-    sort = {'Weekend_Alc_Consumption','Daily_Alc_Consumption','Freetime', 'G3'}
+    sort = ['Weekend_Alc_Consumption','Daily_Alc_Consumption','Freetime', 'G3']
     rep = {'F': 'Female', 'M': 'Male', 'GP': 'Gabriel Pereira', 'MS': 'Mousinho Da Silveira'}
 
     df1 = (   
