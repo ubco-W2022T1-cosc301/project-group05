182f74b5310eabc8dba02619352dc25e1167a22e
Nils-Kelley <77290009+Nils-Kelley@users.noreply.github.com>
2022-11-08 09:29:01 -0800

    Fixed the bug + made some changes for renaming col

--- notebooks/project_functions3.py
+++ notebooks/project_functions3.py
@@ -5,8 +5,9 @@ def load_and_process(url_or_path_to_csv_file):
 
     # Method Chain 1 (Load data and deal with missing data)
    
-    drop_col = ['guardian', 'Medu', 'Fedu', 'reason', 'schoolsup', 'famsup', 'paid',]
-    ren = {'Walc': 'Weekend_Alc_Consumption', 'Dalc': 'Daily_Alc_Consumption', 'freetime': 'Freetime' }
+    drop_col = ['guardian', 'Medu', 'Fedu', 'reason', 'schoolsup', 'famsup', 'paid','Fjob','Mjob', 'famrel', 'famsize', 'Pstatus']
+    ren = {'Walc': 'Weekend_Alc_Consumption', 'Dalc': 'Daily_Alc_Consumption', 'freetime': 'Freetime', 'goout': 'Go_Out',
+    'age': 'Age', 'school':'School', 'sex': 'Sex', 'address': 'Address', 'health': 'Health', 'absences': 'Absences', 'G1': 'Period_1_Grade', 'G2': 'Period_2_Grade', 'G3': 'Final_Grade'}
 
     df1 = (   
     pd.read_csv(url_or_path_to_csv_file, engine='python', on_bad_lines='skip')
