943b56cf11b147966e1a7e0f59fbadc75fc8ff6c
Nils-Kelley <77290009+Nils-Kelley@users.noreply.github.com>
2022-11-08 09:32:53 -0800

    Committing some more changes in function

--- notebooks/project_functions3.py
+++ notebooks/project_functions3.py
@@ -5,7 +5,8 @@ def load_and_process(url_or_path_to_csv_file):
 
     # Method Chain 1 (Load data and deal with missing data)
    
-    drop_col = ['guardian', 'Medu', 'Fedu', 'reason', 'schoolsup', 'famsup', 'paid','Fjob','Mjob', 'famrel', 'famsize', 'Pstatus']
+    drop_col = ['traveltime','failures', 'nursery','guardian', 'Medu', 'Fedu', 'reason', 'schoolsup', 'famsup', 'paid','Fjob','Mjob', 'famrel', 'famsize', 'Pstatus'
+        'higher', 'romantic']
     ren = {'Walc': 'Weekend_Alc_Consumption', 'Dalc': 'Daily_Alc_Consumption', 'freetime': 'Freetime', 'goout': 'Go_Out',
     'age': 'Age', 'school':'School', 'sex': 'Sex', 'address': 'Address', 'health': 'Health', 'absences': 'Absences', 'G1': 'Period_1_Grade', 'G2': 'Period_2_Grade', 'G3': 'Final_Grade'}
 
