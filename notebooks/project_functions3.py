68dc18472421f2614269d268f9fad6e518caeba3
Nils-Kelley <77290009+Nils-Kelley@users.noreply.github.com>
2022-11-08 09:40:08 -0800

    Hopefully this commit solves error

--- notebooks/project_functions3.py
+++ notebooks/project_functions3.py
@@ -6,7 +6,7 @@ def load_and_process(url_or_path_to_csv_file):
     # Method Chain 1 (Load data and deal with missing data)
    
     drop_col = ['traveltime','failures', 'nursery','guardian', 'Medu', 'Fedu', 'reason', 'schoolsup', 'famsup', 'paid','Fjob','Mjob', 'famrel', 'famsize', 'Pstatus'
-        ,'higher', 'romantic', 'internet', 'Address', 'Activities']
+        ,'higher', 'romantic', 'internet', 'address', 'activities']
     ren = {'Walc': 'Weekend_Alc_Consumption', 'Dalc': 'Daily_Alc_Consumption', 'freetime': 'Freetime', 'goout': 'Go_Out',
     'age': 'Age', 'school':'School', 'sex': 'Sex', 'health': 'Health', 'absences': 'Absences', 'G1': 'Period_1_Grade', 'G2': 'Period_2_Grade', 'G3': 'Final_Grade'}
 
