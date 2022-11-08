c629b4341f7d52eb9e8a0a1779eda13eb1171ade
Nils-Kelley <77290009+Nils-Kelley@users.noreply.github.com>
2022-11-08 09:46:04 -0800

    Fixed bug + added replace method to chain

--- notebooks/project_functions3.py
+++ notebooks/project_functions3.py
@@ -8,11 +8,14 @@ def load_and_process(url_or_path_to_csv_file):
     drop_col = ['traveltime','failures', 'nursery','guardian', 'Medu', 'Fedu', 'reason', 'schoolsup', 'famsup', 'paid','Fjob','Mjob', 'famrel', 'famsize', 'Pstatus'
         ,'higher', 'romantic', 'internet', 'address', 'activities']
     ren = {'Walc': 'Weekend_Alc_Consumption', 'Dalc': 'Daily_Alc_Consumption', 'freetime': 'Freetime', 'goout': 'Go_Out',
-    'age': 'Age', 'school':'School', 'sex': 'Sex', 'health': 'Health', 'absences': 'Absences', 'G1': 'Period_1_Grade', 'G2': 'Period_2_Grade', 'G3': 'Final_Grade'}
+    'age': 'Age', 'school':'School', 'sex': 'Sex', 'health': 'Health', 'absences': 'Absences', 'G1': 'Period_1_Grade', 
+    'G2': 'Period_2_Grade', 'G3': 'Final_Grade', 'studytime': 'Study_Time'}
+    rep = {'F': 'Female', 'M': 'Male', 'GP': 'Gabriel Pereira', 'MS': 'Mousinho Da Silveira'}
 
     df1 = (   
     pd.read_csv(url_or_path_to_csv_file, engine='python', on_bad_lines='skip')
         .rename(columns=ren)
+        .replace(rep)
         .drop(drop_col, axis=1)
     )
 
