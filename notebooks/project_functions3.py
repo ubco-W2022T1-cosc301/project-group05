c11cae39b5fda8b87854322787b8854c2da6863c
Nils-Kelley <77290009+Nils-Kelley@users.noreply.github.com>
2022-11-23 12:54:26 -0800

    Committing cleaned df. Working on Tableau rn

--- notebooks/project_functions3.py
+++ notebooks/project_functions3.py
@@ -25,11 +25,21 @@ def load_and_process(url_or_path_to_csv_file):
         .sort_values(sort, ascending=False)
     )
 
-    df2 = (
+    cleaned_data = (
         df1
         .assign(Weekly_Drinking_AVG=df1['Weekend_Alc_Consumption']+df1['Daily_Alc_Consumption']/2)
         .assign(Use_Of_Freetime=df1['Freetime']+df1['Study_Time']+df1['Go_Out']/3)
     )
 
-    return df2
+    return cleaned_data
+
+# Sourced it from Ben's project_Functions2. Thought it was a super useful function.
+def combine_subj(subject_dict):
+   
+    for key, val in subject_dict.items():
+        val['subject'] = key
+    
+    p_dfs = [val for key, val in subject_dict.items()]
+
+    return pd.concat(p_dfs, ignore_index=True)
 
